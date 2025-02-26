92439103e672de966519df10696567c498c1ca55
Patrick Blah <paddyb10@gmail.com>
2025-02-26 15:14:12 +0000

    created difference_days.sql file

--- models/a_b_testing_alpha/differences/difference_days.sql
+++ models/a_b_testing_alpha/differences/difference_days.sql
@@ -10,7 +10,37 @@ control_and_test as (
     select * from control
     union
     select * from test
+),
+
+ranked_data as (
+    select 
+        campaign,
+        day(date) as _date,
+        usd,
+        impressions,
+        reach,
+        clicks,
+        searches,
+        view_content,
+        add_to_cart,
+        purchase,
+        lag(impressions) over (
+                partition by _date 
+                order by campaign
+            ) as prev_count
+ from control_and_test       
 )
 
-select * from control_and_test
-order by DAY(date)
+
+select 
+    campaign,
+    _date,
+    impressions,
+    prev_count,
+    case 
+        when prev_count IS NOT NULL AND prev_count != 0 
+        then impressions - prev_count
+        else NULL 
+    end as diff_impressions
+from ranked_data
+
