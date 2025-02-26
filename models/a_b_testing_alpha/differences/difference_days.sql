e0740398ce5b872947449f4fdb9ace107ff27cbc
Patrick Blah <paddyb10@gmail.com>
2025-02-26 14:25:54 +0000

    changed_control_and_test_raw_data.yml file

--- models/a_b_testing_alpha/differences/difference_days.sql
+++ models/a_b_testing_alpha/differences/difference_days.sql
@@ -1,4 +1,16 @@
 with control as (
     select * from {{ref('stg_control_raw_data')}}
+),
+
+test as (
+    select * from {{ref('stg_test_raw_data')}}
+),
+
+control_and_test as (
+    select * from control
+    union
+    select * from test
 )
-select * from control
+
+select * from control_and_test
+order by DAY(date)
