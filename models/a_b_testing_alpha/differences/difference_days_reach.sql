4f7caabb6dd9e127cc018385fc9e192c314223d3
Patrick Blah <paddyb10@gmail.com>
2025-02-27 12:49:52 +0000

    added difference_days_reach.sql

---
+++ models/a_b_testing_alpha/differences/difference_days_reach.sql
@@ -0,0 +1,52 @@
+with control as (
+    select * from {{ref('stg_control_raw_data')}}
+),
+
+test as (
+    select * from {{ref('stg_test_raw_data')}}
+),
+
+control_and_test as (
+    select * from control
+    union
+    select * from test
+),
+
+ranked_data as (
+    select 
+        campaign,
+        day(date) as _date,
+        usd,
+        impressions,
+        reach,
+        clicks,
+        searches,
+        view_content,
+        add_to_cart,
+        purchase,
+        lag(reach) over (
+        partition by _date 
+        order by campaign
+            ) as prev_count
+ from control_and_test       
+)
+
+
+
+select 
+    campaign,
+    _date,
+    reach,
+    prev_count,
+    case 
+        when prev_count IS NOT NULL AND prev_count != 0 
+        then reach - prev_count
+        else NULL 
+    end as diff_reach
+from ranked_data
+order by _date,campaign
+
+
+
+
+
