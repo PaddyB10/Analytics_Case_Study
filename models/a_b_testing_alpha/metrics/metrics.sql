a479727edc803c133742ed982f0e3b8be376749b
Patrick Blah <paddyb10@gmail.com>
2025-03-01 12:54:17 +0000

    metrics

--- models/a_b_testing_alpha/metrics/metrics.sql
+++ models/a_b_testing_alpha/metrics/metrics.sql
@@ -1,14 +1,16 @@
 with control as (
     select
     *,
-    usd/purchase as "cost_per_purchase"
+    usd/purchase as "cost_per_purchase",
+    searches/purchase as "conversion_rate"
     from {{ref('stg_control_raw_data')}}
 ),
 
 test as (
     select
     *,
-    usd/purchase as "cost_per_purchase"
+    usd/purchase as "cost_per_purchase",
+    searches/purchase as "conversion_rate"
     from {{ref('stg_test_raw_data')}}
 )
 
